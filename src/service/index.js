42f46ec265bd59893562787411d4c07e42aad8c8
Andre Felipe Melo de Oliveira <Andre.infra@alterdata.matriz>
2022-07-20 14:47:05 -0300

    V1.1.1

--- src/service/index.js
+++ src/service/index.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client"
 const apiSecret = process.env.GRAPHQL
 const client = new ApolloClient(
   {
-    uri: `https://api-sa-east-1.graphcms.com/v2/${apiSecret}/master`,
+    uri: `https://api-sa-east-1.graphcms.com/v2/${API_SECRET}/master`,
     cache: new InMemoryCache(),
   }
 )
