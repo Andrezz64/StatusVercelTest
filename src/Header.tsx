16afac70506a69a26702221bd4fc75c09b5d8e19
Andre Felipe Melo de Oliveira <Andre.infra@alterdata.matriz>
2022-07-25 13:31:00 -0300

    1.3.1

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,7 @@ import { Heartbeat } from "phosphor-react"
 export function Header() {
    return(
       <header 
-      className="bg-define-black text-white text-center h-10 flex items-center justify-center flex-row mb-32 ">
+      className="bg-define-black text-white text-center h-10 flex items-center justify-center flex-row mb-20 ">
        Alterdata <p className="ml-2 text-orange-500"> ServiceStatus <Heartbeat size={25} className="inline text-green-400 mb-0.5"/></p>
       </header>
    ) 
